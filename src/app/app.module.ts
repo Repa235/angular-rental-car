e824a218685c551255997d4a5ba149efc703d386
Repa235 <anto.9623.ar@gmail.com>
2022-09-28 17:24:47 +0200

    added components: homepage and my-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { HeaderComponent } from './components/templates/my-header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
