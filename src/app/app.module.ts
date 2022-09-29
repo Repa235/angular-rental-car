2c76b8f7f636bd71db5f4ad551113d0150c23f0a
Repa235 <anto.9623.ar@gmail.com>
2022-09-29 09:20:31 +0200

    homepage, header,footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { HeaderComponent } from './components/templates/my-header/header.component';
+import { MyFooterComponent } from './components/templates/my-footer/my-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    HeaderComponent
+    HeaderComponent,
+    MyFooterComponent
   ],
   imports: [
     BrowserModule
