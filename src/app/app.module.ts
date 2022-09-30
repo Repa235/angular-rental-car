f8f88a23321155b37b58f7a2968d9231130748ce
Repa235 <anto.9623.ar@gmail.com>
2022-09-30 13:00:55 +0200

    tabella utenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './components/homepage/homepage.component';
+import { HomepageComponent } from './views/homepage/homepage.component';
 import { HeaderComponent } from './components/templates/my-header/header.component';
 import { MyFooterComponent } from './components/templates/my-footer/my-footer.component';
-import { LoginFormComponent } from './components/forms/login-form/login-form.component';
+
 
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService} from "./services/in-memory-data.service";
-import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { MyHeaderProfileComponent } from './components/templates/my-header-profile/my-header-profile.component';
+import {MyTableComponent} from "./components/templates/my-table/my-table.component";
+import {MyButtonComponent} from "./components/templates/my-button/my-button.component";
+import {PaginationPipe} from "./components/templates/my-table/pipes/pagination.pipe";
+import {SortPipePipe} from "./components/templates/my-table/pipes/sort-pipe.pipe";
+import { UserListComponent } from './components/lists/user-list/user-list.component';
 
 @NgModule({
   declarations: [
@@ -19,15 +25,20 @@ import { ProfileComponent } from './components/profile/profile.component';
     HomepageComponent,
     HeaderComponent,
     MyFooterComponent,
-    LoginFormComponent,
-    ProfileComponent
+    ProfileComponent,
+    MyHeaderProfileComponent,
+    MyTableComponent,
+    MyButtonComponent,
+    PaginationPipe,
+    SortPipePipe,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'homepage', component: HomepageComponent},
-      {path: 'form-login', component: LoginFormComponent},
       {path: 'profile', component: ProfileComponent},
+
       {path: '', redirectTo: '/homepage', pathMatch: 'full'},
     ]),
     HttpClientModule,
--- src/app/mock-users.ts
+++ src/app/mock-users.ts
@@ -1,6 +1,11 @@
 import {User} from "./models/user";
 
 export const USERS: User[] = [
-  { id:1,  username: 's', name: 'Tito', surname: 'Lare', password: 's', birthday: '', role: 'superuser' },
+ // { id:1,  username: 's', name: 'Tito', surname: 'Lare', password: 's', birthday: '', role: 'superuser' },
   { id:2,  username: 'a', name: 'Oscar', surname: 'Paro', password: 'a', birthday: '', role: 'customer' },
+  { id:3,  username: 'a', name: 'Ugo', surname: 'Losone', password: 'a', birthday: '', role: 'customer' },
+  { id:4,  username: 'a', name: 'Rita', surname: 'Ardata', password: 'a', birthday: '', role: 'customer' },
+  { id:5,  username: 'a', name: 'Paul', surname: 'Kosckyfo', password: 'a', birthday: '', role: 'customer' },
+  { id:6,  username: 'a', name: 'Lina', surname: 'Maya', password: 'a', birthday: '', role: 'customer' },
+  { id:7,  username: 'a', name: 'Luca', surname: 'Pezzolo', password: 'a', birthday: '', role: 'customer' },
 ];
